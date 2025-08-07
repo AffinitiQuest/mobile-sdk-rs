772ff0ecdc25c879354b2836f548a326238e8681
Al Bailey <al@affinitiquest.io>
2025-08-07 15:33:56 -0400

    Properly point the swift package to the latest release

--- Package.swift
+++ Package.swift
@@ -18,12 +18,11 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-//        .binaryTarget(
-//            name: "RustFramework",
-//            url:
-//                "https://github.com/affinitiquest/mobile-sdk-rs/releases/download/1.0.0/RustFramework.xcframework.zip",
-//            checksum: "896c11415e4b7353950dffdc81bbd92336ef8c6f5bfac829cb7dc534ad44ad4c"),
-        .binaryTarget(name: "RustFramework", path: "MobileSdkRs/RustFramework.xcframework"),
+        .binaryTarget(
+            name: "RustFramework",
+            url:
+                "https://github.com/affinitiquest/mobile-sdk-rs/releases/download/1.0.0/RustFramework.xcframework.zip",
+            checksum: "896c11415e4b7353950dffdc81bbd92336ef8c6f5bfac829cb7dc534ad44ad4c"),
         .target(
             name: "SpruceIDMobileSdkRs",
             dependencies: [
